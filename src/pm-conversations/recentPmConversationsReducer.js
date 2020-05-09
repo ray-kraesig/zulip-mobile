88ed604747e062782b472ac5a5bb567e9e29db61
Ray Kraesig <rkraesig@zulip.com>
2020-05-08 18:13:46 -0700

    recent convs: Normalize and retain user IDs received from server.
    The `user_id`s in `recent_private_conversations` do not include the
    self-user. As there are enough distinct ways to express user-lists
    already, we instead adjust this to match `Message#display_recipient`,
    which always includes the self-user (and is the API data source we'll
    most commonly be using alongside this data, anyway).
    
    Archaeological note: the user IDs in `recent_private_conversations`
    were not sorted until 2.2-dev-53-g405a529340 (zulip/zulip@405a529340);
    this was backported to the 2.1 branch as 2.1.1-50-gd452ad31e0
    (zulip/zulip@d452ad31e0).

--- src/pm-conversations/recentPmConversationsReducer.js
+++ src/pm-conversations/recentPmConversationsReducer.js
@@ -5,13 +5,33 @@ import { NULL_ARRAY } from '../nullObjects';
 
 const initialState: RecentPrivateConversationsState = NULL_ARRAY;
 
+const realmInit = (state, action) => {
+  // If this is a pre-2.1 server, we'll have to get by without.
+  if (action.data.recent_private_conversations === undefined) {
+    return initialState;
+  }
+
+  // This part of the API provides lists of user IDs that never include the
+  // self-user. For consistency with Message#display_recipient (and, therefore,
+  // simplicity when using this data), we add them.
+  const self_id: number = action.data.user_id;
+
+  // Note that `user_ids` isn't guaranteed to be sorted prior to v2.1.2; even
+  // if we weren't adding the self-user, we'd have to sort.
+  return action.data.recent_private_conversations.map(({ user_ids, ...rest }) => ({
+    ...rest,
+    user_ids: user_ids.concat(self_id).sort((a, b) => a - b),
+  }));
+};
+
 export default (
   state: RecentPrivateConversationsState = initialState,
   action: Action,
 ): RecentPrivateConversationsState => {
   switch (action.type) {
     case REALM_INIT:
-      return /* action.data.recent_private_conversations || */ initialState;
+      return realmInit(state, action);
+
     default:
       return state;
   }
